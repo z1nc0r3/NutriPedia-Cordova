06076870b0c2713e28167f7e7975b7670a0017d5
z1nc0r3 <manujitha98@gmail.com>
2022-07-25 01:08:12 +0530

    Exception handling functions added.

--- www/js/index.js
+++ www/js/index.js
@@ -4,11 +4,24 @@
 
 // }
 
-let theme_toggler = document.getElementById("theme-button");
-let search_button = document.getElementById('search-button');
+var theme_toggler = document.getElementById("theme-button");
+var search_button = document.getElementById("search-button");
+var result = document.getElementById("result");
+var error = document.getElementById("error");
+var error_message = document.getElementById("error-message");
 
-theme_toggler.addEventListener('click', toggleTheme);
-search_button.addEventListener('click', performSearch);
+theme_toggler.addEventListener("click", toggleTheme);
+search_button.addEventListener("click", performSearch);
+
+document.addEventListener("food_not_found_error", () => {
+    error_message.innerHTML = "Food or Drink not found.";
+    console.log('log');
+    toast();
+});
+document.addEventListener("empty_search_bar_error", () => {
+    error_message.innerHTML = "Query is empty";
+    toast();
+});
 
 if (localStorage.getItem("theme") === "theme-dark") {
     setTheme("theme-dark");
@@ -30,21 +43,21 @@ function toggleTheme() {
 }
 
 function performSearch() {
-    let input = document.getElementById('search-bar').value;
+    let input = document.getElementById("search-bar").value;
 
     const settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://calorieninjas.p.rapidapi.com/v1/nutrition?query=tomato",
-        "method": "GET",
-        "headers": {
+        async: true,
+        crossDomain: true,
+        url: "https://calorieninjas.p.rapidapi.com/v1/nutrition?query=tomato",
+        method: "GET",
+        headers: {
             "X-RapidAPI-Key": "7795701e29msh045cee6ff0afd4ep1560b9jsn3ebc3aa4fb6a",
-            "X-RapidAPI-Host": "calorieninjas.p.rapidapi.com"
-        }
+            "X-RapidAPI-Host": "calorieninjas.p.rapidapi.com",
+        },
     };
-    
-    if (input == '') {
-        console.log('empty search bar.');
+
+    if (input == "") {
+        document.dispatchEvent(new Event("empty_search_bar_error"));
     } else {
         settings.url = `https://calorieninjas.p.rapidapi.com/v1/nutrition?query=${input}`;
         $.ajax(settings).done(function (response) {
@@ -57,12 +70,15 @@ function performSearch() {
 
 function processResponse(response) {
     let element = "";
-    let object = response['items'][0];
+    let object = response["items"][0];
 
-    // console.log(response['items'][0]);
-    for (let [key, value] of Object.entries(object)) {
-        
-        if (key != 'name') {
+    if (object == null) {
+        document.dispatchEvent(new Event("food_not_found_error"));
+        return;
+    }
+
+    for (var [key, value] of Object.entries(object)) {
+        if (key != "name") {
             element += `
             <div class="item d-flex float-right justify-content-between align-items-center">
                 <span id="name">${key}</span>
@@ -70,10 +86,21 @@ function processResponse(response) {
             </div>`;
         }
     }
-    
+
     console.log(element);
-    $('.result').html(element);
+    $(".result").html(element);
+
+    result.style.height = "550px";
 }
+
+function toast() {
+    error.style.opacity = "1";
+
+    setTimeout(() => {
+        error.style.opacity = "0";
+    }, 2500);
+}
+
 
 /* 
 <div class="item d-flex float-right justify-content-between align-items-center">
